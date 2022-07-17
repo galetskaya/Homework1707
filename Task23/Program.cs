cca38250e04164c2739e53a40975d1a6c982f184
galetskaya <li-gn@yandex.ru>
2022-07-17 22:27:47 +0200

    N in cube

---
+++ Task23/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Enter the number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void findQuar(double arg)
+{
+    for(double y = 1; y <= arg; y++)
+    {
+        double a = Math.Pow((y), 3);
+        Console.Write(a);
+        Console.Write(", ");
+    }
+}
+
+
+findQuar(num);
